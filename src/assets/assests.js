4b7e7404533f98a484b1411a6986ef4543484493
Anand <anand.work2005@gmail.com>
2025-08-15 16:07:12 +0530

    add image and title in the navbar

---
+++ src/assets/assests.js
@@ -0,0 +1,5 @@
+import monkey_type from './monkeytype.svg'
+
+export const assets = {
+  monkey_type
+}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,17 @@
+import { assets } from "../../assets/assests"
+import "./Navbar.css";
+
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="leftSide"></div> 
+      <div className="leftSide">
+        <div className="logo">
+          <img src={assets.monkey_type} alt="logo" className="logo" />
+        </div>
+        <div className="title">
+          <h1>monkeytype</h1>
+        </div>
+      </div> 
       <div className="rightSide"></div> 
     </div>
   )
