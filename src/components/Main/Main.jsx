31b34d77dfd595cd8e409b53e41ad2b4800bd4f4
Anand <anand.work2005@gmail.com>
2025-08-15 22:57:05 +0530

    working on the word generate in the main div

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { generate } from "random-words";
+import "./Main.css";
+import { FaArrowRotateRight } from "react-icons/fa6";
+
+function GenerateSentence({ wordCount = 29 }) {
+  const [sentence, setSentence] = useState(() =>
+    generate({ exactly: wordCount, join: " " })
+  );
+
+  const regenerate = () => {
+    setSentence(generate({ exactly: wordCount, join: " " }));
+  };
+
+  return (
+    <div className="words-container">
+      <p className="words">{sentence}</p>
+      <button className="regenerate-btn" onClick={regenerate}>
+        <FaArrowRotateRight className="navIcons" />
+      </button>
+    </div>
+  );
+}
+
+function Main() {
+  return (
+    <div className="main">
+      <div className="main-one">
+        <div className="options"></div>
+      </div>
+      <GenerateSentence />
+    </div>
+  );
+}
+
+export default Main;
+
