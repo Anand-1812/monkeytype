ed632e2ad084c3cfb35ff036c26b7508579dfe8a
Anand <anand.work2005@gmail.com>
2025-08-15 21:39:45 +0530

    header part complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import { assets } from "../../assets/assests"
 import "./Navbar.css";
 import { FaKeyboard } from "react-icons/fa";
+import { FaCrown } from "react-icons/fa6";
+import { TbCandle } from "react-icons/tb";
+import { IoMdSettings } from "react-icons/io";
 
 function Navbar() {
   return (
@@ -9,9 +12,18 @@ function Navbar() {
         <div className="logo">
           <img src={assets.monkey_type} alt="logo" className="navIcons" />
         </div>
-        <FaKeyboard className="navIcons" />
-      </div> 
-      <div className="rightSide"></div> 
+        <div className="title">
+          <div className="one">monkey see</div>
+          <div className="two">monkeytype</div>
+        </div>
+        <nav className="nav">
+          <FaKeyboard className="navIcons" />
+          <FaCrown className="navIcons" />
+          <TbCandle className="navIcons" />
+          <IoMdSettings className="navIcons" />
+        </nav>
+      </div>
+      <div className="rightSide"></div>
     </div>
   )
 }
