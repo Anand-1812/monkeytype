26724b366862a4c3b833ac4f11d3c79d5286723f
Anand <anand.work2005@gmail.com>
2025-08-15 15:28:22 +0530

    add the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 function App() {
-  return (
-    <h1>Monkey type</h1>
-  );
 }
 
 export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="leftSide"></div> 
+      <div className="rightSide"></div> 
+    </div>
+  )
+}
+
+export default Navbar
