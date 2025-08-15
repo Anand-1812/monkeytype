a82c01991e3d836c5da3d361484dfd4683e2baa1
Anand <anand.work2005@gmail.com>
2025-08-15 21:06:10 +0530

    add the logo, so much pain to find

--- src/assets/assests.js
+++ src/assets/assests.js
@@ -1,4 +1,4 @@
-import monkey_type from './monkeytype.svg'
+import monkey_type from './monkeytype.png'
 
 export const assets = {
   monkey_type
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,15 @@
 import { assets } from "../../assets/assests"
 import "./Navbar.css";
+import { FaKeyboard } from "react-icons/fa";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide">
         <div className="logo">
-          <img src={assets.monkey_type} alt="logo" className="logo" />
+          <img src={assets.monkey_type} alt="logo" className="navIcons" />
         </div>
-        <div className="title">
-          <h1>monkeytype</h1>
-        </div>
+        <FaKeyboard className="navIcons" />
       </div> 
       <div className="rightSide"></div> 
     </div>
