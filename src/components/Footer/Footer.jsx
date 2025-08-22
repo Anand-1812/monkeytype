c1f05189a789a7e9c6bc6e7e8133afcf3166641f
Anand <anand.work2005@gmail.com>
2025-08-22 18:52:28 +0530

    add the footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="left">
+        <p className="copyright">Copyright (c) 2025 Author. All Rights Reserved.</p>
+      </p>
+
+    </div>
+  )
+}
+
+export default Footer
