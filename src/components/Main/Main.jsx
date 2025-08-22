3b172c436e5a9952fc563044608435882c92e31d
Anand <anand.work2005@gmail.com>
2025-08-22 19:59:37 +0530

    cusor tracking done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { generate } from "random-words";
 import "./Main.css";
 import { FaArrowRotateRight } from "react-icons/fa6";
@@ -7,12 +7,14 @@ function GenerateSentence({ wordCount = 29 }) {
   const [sentence, setSentence] = useState(() =>
     generate({ exactly: wordCount, join: " " })
   );
-
   const [currIndex, setCurrIndex] = useState(0);
 
+  const charRefs = useRef([]);
+  const cursorRef = useRef(null);
+  const containerRef = useRef(null);
   useEffect(() => {
-    const handleKeyDown = (e) => {
-      setCurrIndex((prev) => Math.min(prev + 1, sentence.length - 1));
+    const handleKeyDown = () => {
+      setCurrIndex((prev) => Math.min(prev + 1, sentence.length));
     };
 
     window.addEventListener("keydown", handleKeyDown);
@@ -20,7 +22,13 @@ function GenerateSentence({ wordCount = 29 }) {
   }, [sentence]);
 
   useEffect(() => {
-    console.log("currIndex updated:", currIndex);
+    if (charRefs.current[currIndex] && cursorRef.current && containerRef.current) {
+      const rect = charRefs.current[currIndex].getBoundingClientRect();
+      const parentRect = containerRef.current.getBoundingClientRect();
+
+      cursorRef.current.style.left = rect.left - parentRect.left + "px";
+      cursorRef.current.style.top = rect.top - parentRect.top + "px";
+    }
   }, [currIndex]);
 
   const regenerate = () => {
@@ -29,14 +37,18 @@ function GenerateSentence({ wordCount = 29 }) {
   };
 
   return (
-    <div className="words-container">
+    <div className="words-container" ref={containerRef}>
       <p className="words">
         {sentence.split("").map((ch, i) => (
-          <span key={i} className="char">
-            {i === currIndex && <span className="cursor" />}
+          <span
+            key={i}
+            ref={(el) => (charRefs.current[i] = el)}
+            className="char"
+          >
             {ch}
           </span>
         ))}
+        <span ref={cursorRef} className="cursor" />
       </p>
       <button className="regenerate-btn" onClick={regenerate}>
         <FaArrowRotateRight className="navIcons" />
